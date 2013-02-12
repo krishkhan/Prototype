1b60303a883c7a3cf086bdb9188c529294455f67
vamc3132 <vamsikrishna.yesu@gmail.com>
2013-02-12 07:19:44 +0530

    Updated files

---
+++ UsecaseScreenShots/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UsecaseScreenShots
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ConnectionViewModel _cvModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _cvModel = new ConnectionViewModel();
+            this.DataContext = _cvModel;
+        }
+    }
+
+    public class ConnectionViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand ConnectCommand { get; set; }
+
+        public ConnectionViewModel()
+        {
+            this.ConnectCommand = new ConnectCommand(this);
+        }
+
+        public bool Connect()
+        {
+            Boolean connectionStatus = false;
+            MessageBox.Show("Connection Initiated");
+            return connectionStatus;
+        }
+
+    }
+
+    public class ConnectCommand : ICommand
+    {
+        private readonly ConnectionViewModel _cvm;
+
+        public ConnectCommand(ConnectionViewModel connectionViewModel)
+        {
+            _cvm = connectionViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _cvm.Connect();
+        }
+    }
+
+    public static class CommsCommands
+    {
+        public static RoutedCommand ConnectToPort = new RoutedCommand();
+    }
+
+    public class SynchronousSocketClient
+    {
+
+        public static void StartClient() {
+        // Data buffer for incoming data.
+        byte[] bytes = new byte[1024];
+
+        // Connect to a remote device.
+        try {
+            // Establish the remote endpoint for the socket.
+            // This example uses port 11000 on the local computer.
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());  
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress,11000);
+
+            // Create a TCP/IP  socket.
+            Socket sender = new Socket(ipAddress.AddressFamily, 
+                SocketType.Stream, ProtocolType.Tcp );
+
+            // Connect the socket to the remote endpoint. Catch any errors.
+            try {
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}",
+                    sender.RemoteEndPoint.ToString());
+
+                // Encode the data string into a byte array.
+                byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                // Send the data through the socket.
+                int bytesSent = sender.Send(msg);
+
+                // Receive the response from the remote device.
+                int bytesRec = sender.Receive(bytes);
+                Console.WriteLine("Echoed test = {0}",
+                    Encoding.ASCII.GetString(bytes,0,bytesRec));
+
+                // Release the socket.
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+                
+            } catch (ArgumentNullException ane) {
+                Console.WriteLine("ArgumentNullException : {0}",ane.ToString());
+            } catch (SocketException se) {
+                Console.WriteLine("SocketException : {0}",se.ToString());
+            } catch (Exception e) {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        } catch (Exception e) {
+            Console.WriteLine( e.ToString());
+        }
+    }
+
+      
+    }
+      
+}
